622680d1c67226fd9f9df01dd487b9feb0b3da5a
AnonyMnaUmouS <99983872+AnonyMnaUmouS@users.noreply.github.com>
2024-04-14 19:37:44 +0200

    update (backup)

--- bak.py
+++ bak.py
@@ -9,6 +9,7 @@ from tkinter import messagebox, Button, PhotoImage, Label
 
 class GraphApp:
     def __init__(self, root):
+        self.button_back_id = None
         self.enter_edges = None
         self.edges_label = None
         self.enter_nodes = None
@@ -23,7 +24,7 @@ class GraphApp:
         self.nodes = []  # To store node IDs and coordinates
         self.edges = []  # To store edge IDs
 
-        img = PhotoImage(file='C:\\Users\\Tima\\Desktop\\Bakalarka\\Práca\\new_graph.png')
+        img = PhotoImage(file='new_graph.png')
         self.canvas.create_image(200, 200, image=img, tags="image_button1")
         self.canvas.tag_bind("image_button1", "<Button-1>", self.new_graph)
         self.canvas.bind('<B1-Motion>', self.move)
@@ -32,11 +33,17 @@ class GraphApp:
         self.root.mainloop()
 
     def klik(self, event):
-        # print(event.x, event.y)
         ...
 
     def move(self, event):
-        ...
+        index = -1
+        for i in range(len(self.positions)):
+            if abs(self.positions[i][0] - event.x) < 15 and abs(self.positions[i][1] - event.y) < 15:
+                index = i
+                break
+        if index != -1:
+            self.positions[index] = (event.x, event.y)
+            self.draw()
 
     def button_click(self, a):
         if self.enter_nodes.get() == "":
@@ -65,40 +72,48 @@ class GraphApp:
         self.enter_edges.destroy()
 
         nodes = nodes.split(",")
-        for i in range(len(nodes)):
-            nodes[i] = int(nodes[i])
+        # for i in range(len(nodes)):
+        #     nodes[i] = nodes[i]
 
-        edges = edges.split()
-
-        for i in range(len(edges)):
-            edges[i] = edges[i].split(",")
-            edges[i][0] = int(edges[i][0])
-            edges[i][1] = int(edges[i][1])
-            edges[i] = tuple(edges[i])
+        if "(" in edges:
+            edges = edges[1:-1].replace('), ', '),').split('),(')
+            edges = [tuple(map(str, t.replace(' ', '').split(','))) for t in edges]
+            # print(edges)
+        else:
+            edges = edges.split()
+            for i in range(len(edges)):
+                edges[i] = edges[i].split(",")
+                # edges[i][0] = edges[i][0]
+                # edges[i][1] = edges[i][1]
+                edges[i] = tuple(edges[i])
 
         g = nx.Graph()
         g.add_nodes_from(nodes)
         g.add_edges_from(edges)
 
+        # print(g.nodes)
+        # print(g.edges)
+
         if token:
             g = self.to_token(g)
 
-        self.draw(g)
+        self.prepare_to_draw(g)
 
-    def draw1(self, g):
-        p = nx.spring_layout(g)
+    # def draw1(self):
+    #     p = nx.spring_layout(g)
+    #
+    #     fig, ax = plt.subplots(figsize=(4, 4))
+    #     nx.draw(g, p, ax=ax, with_labels=True, node_size=700, node_color='skyblue', font_size=12, font_weight='bold',
+    #             width=2)
+    #
+    #     canvas = FigureCanvasTkAgg(fig, master=self.root)
+    #     canvas.draw()
+    #
+    #     canvas.get_tk_widget().place(x=0, y=0)
 
-        fig, ax = plt.subplots(figsize=(4, 4))
-        nx.draw(g, p, ax=ax, with_labels=True, node_size=700, node_color='skyblue', font_size=12, font_weight='bold',
-                width=2)
-
-        canvas = FigureCanvasTkAgg(fig, master=self.root)
-        canvas.draw()
-
-        canvas.get_tk_widget().place(x=0, y=0)
-
-    def draw(self, g):
+    def prepare_to_draw(self, g):
         layout = nx.spring_layout(g, seed=42)
+        self.nodes, self.edges, self.positions = [], [], []
 
         for node in g.nodes():
             x, y = layout[node]
@@ -106,6 +121,21 @@ class GraphApp:
             self.nodes.append(node)
 
         self.edges = list(g.edges)
+
+        self.draw()
+
+        img = PhotoImage(file='back.png')
+        self.button_back_id = self.canvas.create_image(400 - img.width() / 2, 400 - img.height() / 2,
+                                                       image=img, tags="button_back")
+        self.canvas.tag_bind(self.button_back_id, "<Button-1>", self.button_back)
+
+        self.root.mainloop()
+
+    def draw(self):
+
+        for item in self.canvas.find_all():
+            if item != self.button_back_id:
+                self.canvas.delete(item)
 
         for x, y in self.edges:
             x = self.positions[self.nodes.index(x)]
@@ -117,30 +147,39 @@ class GraphApp:
             self.canvas.create_oval(pos[0] - 15, pos[1] - 15, pos[0] + 15, pos[1] + 15, fill="#50a5fa", outline="")
             self.canvas.create_text(pos[0], pos[1], text=str(self.nodes[i]), font=("Arial", 12), fill="black")
 
-        self.root.mainloop()
+        # img = PhotoImage(file='back.png')
+        # self.canvas.create_image(400 - img.width() / 2, 400 - img.height() / 2, image=img,
+        #                          tags="button_back")
+        # self.canvas.tag_bind("button_back", "<Button-1>", self.button_back)
+        # self.root.mainloop()
 
-    def new_graph(self, a):
+    def button_back(self, event):
+        self.new_graph(event, ', '.join(map(str, self.nodes)), ', '.join(f"({a}, {b})" for a, b in self.edges))
+
+    def new_graph(self, a, nodes="", edges=""):
         # print(a)
         self.canvas.delete('all')
         custom_font = ('Arial', 12)
         self.nodes_label = tk.Label(font=custom_font, text='Enter nodes:')
         self.nodes_label.place(x=20, y=20)
         self.enter_nodes = tk.Entry(width=60)
+        self.enter_nodes.insert(tk.END, nodes)
         self.enter_nodes.place(x=20, y=50)
 
         self.edges_label = tk.Label(font=custom_font, text='Enter edges:')
         self.edges_label.place(x=20, y=100)
         self.enter_edges = tk.Text(self.root, width=45, height=5)
+        self.enter_edges.insert(tk.END, edges)
         self.enter_edges.place(x=20, y=130)
 
         # enter_edges = tk.Entry(width=60)
         # enter_edges.place(x=20, y=130)
 
-        img1 = PhotoImage(file='C:\\Users\\Tima\\Desktop\\Bakalarka\\Práca\\create_graph.png')
+        img1 = PhotoImage(file='create_graph.png')
         self.canvas.create_image(20 + img1.width() / 2, 230 + img1.height() / 2, image=img1, tags="button1")
         self.canvas.tag_bind("button1", "<Button-1>", self.button_click)
 
-        img2 = PhotoImage(file='C:\\Users\\Tima\\Desktop\\Bakalarka\\Práca\\create_token_graph.png')
+        img2 = PhotoImage(file='create_token_graph.png')
         self.canvas.create_image(20 + img1.width() + img2.width() / 2, 230 + img2.height() / 2, image=img2,
                                  tags="button2")
         self.canvas.tag_bind("button2", "<Button-1>", self.button_click_token)
